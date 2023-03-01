1550f8c49183a34d6005051357b90af31b098b47
Darshan Satone <darshmac@Darshans-MacBook-Pro.local>
2023-03-02 02:08:08 +0530

    Medal Count Complete

--- helper.py
+++ helper.py
@@ -1,5 +1,33 @@
 import numpy as np
 
+def fetch_medal_tally(df, year, country):
+    medal_df = df.drop_duplicates(subset = ['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
+    flag = 0
+    if year == 'Overall' and country == 'Overall':
+        temp_df = medal_df
+    if year == 'Overall' and country != 'Overall':
+        flag = 1
+        temp_df = medal_df[medal_df['region'] == country]
+    if year != 'Overall' and country == 'Overall':
+        temp_df = medal_df[medal_df['Year'] == int(year)]
+    if year != 'Overall' and country != 'Overall':
+        temp_df =  medal_df[(medal_df['Year'] == 2016) & (medal_df['region'] == country)] 
+    
+    if flag == 1:
+        x =  temp_df.groupby('Year').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Year', ascending = True).reset_index()
+    else:    
+        x =  temp_df.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending = False).reset_index()
+    
+    x['total'] = x['Gold'] + x['Silver'] + x['Bronze']
+    
+    x['Gold'] = x['Gold'].astype('int')
+    x['Silver'] = x['Silver'].astype('int')
+    x['Bronze'] = x['Bronze'].astype('int')
+    x['total'] = x['total'].astype('int')
+    
+    
+    return x
+
 def medal_tally(df):
     medal_tally = df.drop_duplicates(subset = ['Team', 'NOC', 'Games', 'Year', 'City', 'Sport', 'Event', 'Medal'])
     medal_tally = medal_tally.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values('Gold', ascending = False).reset_index()
@@ -8,7 +36,7 @@ def medal_tally(df):
     medal_tally['Gold'] = medal_tally['Gold'].astype('int')
     medal_tally['Silver'] = medal_tally['Silver'].astype('int')
     medal_tally['Bronze'] = medal_tally['Bronze'].astype('int')
-    medal_tally['Total'] = medal_tally['Total'].astype('int')
+    medal_tally['total'] = medal_tally['total'].astype('int')
     
     return medal_tally
 
