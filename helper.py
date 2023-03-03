588c88532065876e04a15a4221d4655f2529aab5
Darshan Satone <darshmac@Darshans-MacBook-Pro.local>
2023-03-03 17:29:10 +0530

    Overall Analysis Complete

--- helper.py
+++ helper.py
@@ -56,3 +56,13 @@ def data_over_time(df, col):
     nations_over_time.rename(columns={'index':'Edition', 'Year':col}, inplace = True)
     
     return nations_over_time
+
+def most_successful(df, sport):
+    temp_df = df.dropna(subset = ['Medal'])
+    
+    if sport != 'Overall':
+        temp_df = temp_df[temp_df['Sport'] == sport]
+    
+    x = temp_df['Name'].value_counts().reset_index().head(15).merge(df, left_on='index', right_on='Name', how='left')[['index','Name_x','Sport','region']].drop_duplicates('index')
+    x.rename(columns={'index':'Name','Name_x':'Medals'},inplace=True)
+    return x
